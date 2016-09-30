cc71b1baeb1cf3de5b343c28b0228edba1aad035
Yogita Nain <nain.yogita@gmail.com>
2016-09-30 14:52:15 +0530

    directives

--- client/controllers/todo.js
+++ client/controllers/todo.js
@@ -1,37 +1,44 @@
-angular.module('todoApp', []).controller('TodoListController', function() {
-    var vm = this;
+(function() {
+    //  var a = 5;
+    var app = angular.module('todoController', []);
 
-    vm.todos = [{
-        text: 'task one',
-        done: true
-    }, {
-        text: 'task two',
-        done: false
-    }, {
-        text: 'tast three',
-        done: false
-    }];
-
-    vm.addTodo = function() {
-        vm.todos.push({
-            text: vm.todoText,
+    app.controller('TodoListController', function() {
+        //Contoller starts with a Upper Case, second param is an anonymous function
+        //vm ..model view, defines scope
+        var vm = this;
+        //  vm.a = a;
+        vm.todos = [{
+            text: 'task one',
+            done: true
+        }, {
+            text: 'task two',
             done: false
-        });
-        vm.todoText = '';
-    };
+        }, {
+            text: 'tast three',
+            done: false
+        }];
 
-    vm.remaining = function() {
-        var count = 0;
-        angular.forEach(vm.todos, function(todo) {
-            count += todo.done ? 0 : 1;
-        });
-        return count;
-    }
+        vm.addTodo = function() {
+            vm.todos.push({
+                text: vm.todoText,
+                done: false
+            });
+            vm.todoText = '';
+        };
 
-    vm.clearCompleted = function() {
-        console.log("clear completed");
-        vm.todos = vm.todos.filter(function(todo) {
-            return !todo.done;
-        });
-    };
-});
+        vm.remaining = function() {
+            var count = 0;
+            angular.forEach(vm.todos, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        }
+
+        vm.clearCompleted = function() {
+            console.log("clear completed");
+            vm.todos = vm.todos.filter(function(todo) {
+                return !todo.done;
+            });
+        };
+    });
+})();
---
+++ client/modules/app.js
@@ -0,0 +1,40 @@
+//Wrapping javascript in closure is a good habit
+(function() {
+    var a = 5; //variale declared outside module accessed inside controller
+
+    //first param is module name, second param is dependecies on other modules
+    var app = angular.module('app', ['todoController']);
+
+    //Custome Directives
+    //can also use ng-include , but best to use expanding directive
+    //Template Expanding directive, E---- > Element
+    //A -->Attribute directive <h3 prodcut-title></h3>
+
+    app.directive('productInfo', function() {
+        return {
+            restrict: 'E', //Type of Directive, E for element
+            templateUrl: './directives/product-info.html'
+        };
+    });
+
+    //Syntax for using services
+    app.controller('serviceExample', ['$http', function($http) {
+        var vm = this;
+        //initialize to an empty array since page renders before data returns
+        vm.a = [];
+        $http.get('/abc.json').success(function(data) {
+            //do something with data;
+
+            //WRONG --> this inside callback is the service
+            //this.a = data;
+
+            vm.a = data; //async request to fetch value of a , might take some time... initialize a so that empty a is loaded
+        });
+    }]);
+
+
+})();
+
+//app.js is top level module
+//dependencies on controller modules and directive modules
+//split modules on basis of functionalities
